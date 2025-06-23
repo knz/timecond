4cedff16e9ddbeef831a557886ffe8c13d1b7a04
Raphael 'kena' Poss <knz@thaumogen.net>
2025-06-23 19:12:55 +0200

    link timecond via alias

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@knz/timecond': path.resolve(__dirname, '../../packages/timecond/src'),
     },
   },
 });
