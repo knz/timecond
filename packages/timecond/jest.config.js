f27271bd298c36b93a1571714440abcacc2c3375
Raphael 'kena' Poss <knz@thaumogen.net>
2025-06-23 17:31:29 +0200

    fix support for tests in linter

--- packages/timecond/jest.config.js
+++ packages/timecond/jest.config.js
@@ -1,11 +1,8 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
-module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
+export default {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverage: true,
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 };
